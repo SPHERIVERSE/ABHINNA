5121ea7bb73276a32abec4cc19dc119e651cc358
SPHERIVERSE <spheri20verse04@gmail.com>
2026-01-07 16:11:50 +0530

    next config updated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
       {
         // When you fetch('/api/courses'), it goes to http://localhost:4000/api/courses
         source: '/api/:path*',
-        destination: 'http://localhost:4000/api/:path*', 
+        destination: 'http://localhost:4000/:path*', 
       },
       {
         // For your faculty/course images stored in the uploads folder
